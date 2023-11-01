d62e5b2af8e136b1ca3e6380bb6dfae9ec4ec1fb
SamuExe14 <samueleleoonardi14@gmail.com>
2023-11-01 11:39:51 +0100

    Nuovo file 2,18.c

---
+++ Prog1/2,18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (){
+    int x;
+    int y;
+    
+    puts("Enter two numbers:");
+        scanf("%d %d", &x, &y);
+
+    if (x > y){
+        printf("%d is larger than %d\n", x, y);
+    }
+    if (x < y ){
+        printf("%d is larger than %d\n", y, x);
+    }
+    if (x == y){
+        printf("%d and %d are equals\n", x, y);
+    }
+}
