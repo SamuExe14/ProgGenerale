2e86ad41c3b2a7b53da246b68f5d7c37c9be9d4b
SamuExe14 <samueleleoonardi14@gmail.com>
2023-11-01 17:14:32 +0100

    Creato file 2,32.c

---
+++ Prog1/2,32.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float altezza;
+    float peso_Kg;
+
+    puts("Inserire in ordine altezza in metri e peso in Kg");
+    scanf("%f%f", &altezza, &peso_Kg);
+
+    float BMI = peso_Kg / (altezza * altezza);
+    printf("\nIl  BMI risultante mostra il seguente risultato: %.2f\n\n", BMI);
+
+    puts("\nVALORI DEL BMI");
+    puts("Sottopeso:\tmeno di 18.5");
+    puts("Normale:\ttra 18.5 e 24.9");
+    puts("Sovrappeso:\ttra 25 e 29.9");
+    puts("Obeso:\t30 o oltre");
+}
