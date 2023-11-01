da2c922e8c443d7ea064dc5b27487ff599bcdcb9
SamuExe14 <samueleleoonardi14@gmail.com>
2023-11-01 16:41:45 +0100

    Creato file 2,20.c

---
+++ Prog1/2,20.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+int main (){
+    float raggio;
+    float pi = 3.14159;
+    
+    puts("Inserire il raggio del cerchio");
+    scanf("%f", &raggio);
+
+    float area = pow(raggio,2) * pi;
+    printf("L'area del cerchio è %.2f cm\n", area);
+
+    float circonferenza = (2 * pi) * raggio;
+    printf("La circonferenza è %.2f cm\n", circonferenza);
+
+    float diametro = raggio * 2;
+    printf("Il diametro del cerchio è %.2f cm\n", diametro);
+}
+
+
