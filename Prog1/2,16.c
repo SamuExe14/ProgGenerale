14818ac16a39430d8817d71dc3f5cc1005118904
SamuExe14 <samueleleoonardi14@gmail.com>
2023-11-01 10:56:07 +0100

    Modificato .gitignore

---
+++ Prog1/2,16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+    int num1;
+    int num2;
+    
+    puts("Enter the first number: ");
+    scanf("%d", &num1);
+
+    puts("Ente the second number: ");
+    scanf("%d", &num2);
+
+    printf("The sum is %d\n", num1 + num2);
+    printf("The product is %d\n", num1 * num2);
+    printf("The difference is %d with rest %d\n",num1 / num2, num1 % num2);
+}
+
