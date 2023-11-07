8ca5bbf58b7bc330748145aa90b8b27a83412138
Samuele Leonardi <samueleleoonardi14@gmail.com>
2023-11-07 16:07:53 +0100

    Modificato file 4,10.c

--- Prog1/Esercizi/4,9.c
+++ Prog1/Esercizi/4,9.c
@@ -4,7 +4,6 @@ int main(){
      unsigned int num_elementi;
      int somma = 0;
      int appoggio;
-     float media;
 
      printf("Inserire il numero di elementi che si vogliono inserire: ");
      scanf("%u", &num_elementi);
@@ -15,8 +14,8 @@ int main(){
           
           scanf("%d", &appoggio);
           somma += appoggio;
+          
      }
-          media = somma / num_elementi;
 printf("La somma dei tuoi numeri è: %d\n", somma);
-printf("La media dei numeri è: %.2f", media);
+
 }
