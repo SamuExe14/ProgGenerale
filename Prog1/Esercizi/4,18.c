ea3be1b59b2ac4eec9d9e748ab33f508717b8d39
SamuExe14 <samu@PCoddio.home-life.hub>
2023-11-10 18:08:26 +0100

    Creato file 4,18.c

---
+++ Prog1/Esercizi/4,18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+     int ast;
+
+     printf("Inserire il numero di * da inserire nell'app: ");
+     scanf("%d",&ast);
+
+     for (int i = 1; i <= ast; ++i){
+          printf("*");
+     }
+     puts("");
+}
