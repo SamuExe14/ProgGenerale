d4e46693df6c5970a6285632cae5db84ee2750db
SamuExe14 <samu@PCoddio.home-life.hub>
2023-11-14 21:21:42 +0100

    Creato file math.c e funzioni.c

---
+++ Prog1/Libro/math,h.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+//! tutte le funzioni che fanno parte della libreria math.h restituiscono un valore double
+
+printf("%.2f\n", sqrt(900)); //-> calcola la radice quadrata sqrt(x)
+printf("%f", pow(5, 2)); // -> calcola la potenza di tra due numeri: x^y = pow( x, y)
+ceil(x) //-> arrotonda x all'intero più piccolo non minore di x
+floor(x) //-> arrotonda x all'intero più grande non maggiore di x
+
+//! Un programma con molte funzioni scritte in maniera corretta è più riutilizzabile e più efficiente
+}
