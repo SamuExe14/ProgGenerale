9f8076ca9bf5afeebe87e7829bca2e71dad7e8e3
Samuele Leonardi <samueleleoonardi14@gmail.com>
2023-11-16 08:48:35 +0100

    Creato il file rand.c

--- { Prog1 }/Libro/rand.c
+++ { Prog1 }/Libro/rand.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h> //! contiene la funzione rand() al suo interno
+#include <stdlib.h> //! contiene la funzione rand() e srand() al suo interno
 
 int main()
-{
-     int i = rand(); //-> assegna un valore a i da 0 a RAND_MAX
-     int x = 1 + rand() % 6; //-> assegna un valore a x da 0 a 6
-     
-     
+{            //↓-> la funzione rand genera numeri pseudocasuali
+     int i = rand(); //* -> assegna un valore a i da 0 a RAND_MAX
+     int x = 1 + rand() % 6; //* -> assegna un valore a x da 0 a 6
+//-------------------------------------------------------------------------//
+
+     unsigned int seed;
+     printf("Enter the seed: \n");
+     scanf("%d", &seed);
+     srand(seed) //* -> srand fornisce un seme a rand() per la generazione dei numeri casuali
+
+     srand(time(NULL)) //! -> legge l'orologio di sistema e lo fornisce come seme per rand().
 }
