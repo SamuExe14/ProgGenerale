974e288182e3257b7ae5723b9e8ada2f3be2053d
SamuExe14 <samu@PCoddio.home-life.hub>
2023-11-17 17:23:14 +0100

    Aggiunto il file 5,15 e math.c

---
+++ { Prog1 }/Esercizi/5,15.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+double hypotenuse(double x, double y){
+     return x, y;
+
+}
+
+int main(){
+
+     double cateto_M;
+     double cateto_m;
+
+     printf("Inserire cateto maggiore e cateto minore: \n");
+     scanf("%f %f", &cateto_M, &cateto_m);
+
+     printf("L'ipotenusa è %e %e", cateto_M, cateto_m);
+}
+
--- { Prog1 }/Libro/math,h.c
+++ { Prog1 }/Libro/math,h.c
@@ -7,8 +7,8 @@ int main(){
 
 printf("%.2f\n", sqrt(900)); //-> calcola la radice quadrata sqrt(x)
 printf("%f", pow(5, 2)); // -> calcola la potenza di tra due numeri: x^y = pow( x, y)
-ceil(x) //-> arrotonda x all'intero più piccolo non minore di x
-floor(x) //-> arrotonda x all'intero più grande non maggiore di x
+ceil(4); //-> arrotonda x all'intero più piccolo non minore di x
+floor(4); //-> arrotonda x all'intero più grande non maggiore di x
 
 //! Un programma con molte funzioni scritte in maniera corretta è più riutilizzabile e più efficiente
 }
