7503829823cb3ad363edb5a1aaef858433f4274f
SamuExe14 <samu@PCoddio.home-life.hub>
2023-11-21 10:11:14 +0100

    Creato il file bidimensionalArray.c

---
+++ {Prog1}/Libro/bidimensionalArray.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void printArray(int a[2][3]) {
+     for(size_t i = 0; i <= 1; ++i){
+          for(size_t j = 0; j <= 2; ++j){
+               printf("%d ", a[i][j]);
+          }
+          puts("");
+     }
+}
+
+int main()
+{
+     int array1[2][3] = {{1, 2, 3}, {4, 5, 6}};
+     puts("Values in array1 by row are:");
+     printArray(array1);
+
+     int array2[2][3] = {1, 2, 3, 4, 5};
+     puts("Values in array2 by row are:");
+     printArray(array2);
+
+     int array3[2][3] = {{1, 2}, {4}};
+     puts("Values in array3 by row are:");
+     printArray(array3);
+}
