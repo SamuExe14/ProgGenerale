d7bef3df3fd0da5d23836fc44d50be5ac13ae263
SamuExe14 <samu@PCoddio.home-life.hub>
2023-11-28 17:54:13 +0100

    Creato file 1.c

--- {Prog1}/marryChristmas/1.c
+++ {Prog1}/marryChristmas/1.c
@@ -1,1 +1,41 @@
+/*
+Un santone induista si trova nella regione indiana del
+Rajasthan, quest'ultima simulata con una matrice
+quadrata di boolean in cui il valore true indica la
+presenza di un libro sacro. Scrivere una funzione che
+prenda in input una matrice di boolean e restituisca il
+numero di libri raccolti dal santone che, partendo dal
+centro della regione, esegue un movimento casuale
+unitario alla volta in una delle quattro direzioni prima
+che egli sia traviato dalle usanze oltre colonne.
+*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#define ARRAY 7
+
+void randomizza(bool array[ARRAY][ARRAY], size_t size)
+{
+     for (size_t row = 0; row < ARRAY; ++row)
+     {
+          puts("");
+          for (size_t column = 0; column < ARRAY; ++column)
+          {
+               array[row][column] = rand() % 2;
+               printf("[%d] ", array[row][column]);
+          }
+     }
+     puts("");
+}
+
+void *randomPtr(randomizza(bool array[ARRAY][ARRAY]))
+{
+}
+
+int main()
+{
+     bool rajasthan[ARRAY][ARRAY] = {0};
+
+     randomizza(rajasthan);
+}
