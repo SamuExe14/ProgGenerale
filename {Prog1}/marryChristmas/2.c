0563df0b3efd4d7af9733f53e86d0df4ba9040f6
SamuExe14 <samu@PCoddio.home-life.hub>
2023-12-05 19:35:51 +0100

    Modificato .gitignore e aggiunto marryChristmas.c

---
+++ {Prog1}/marryChristmas/2.c
@@ -0,0 +1,8 @@
+/*Due stringhe si dicono sorelle se hanno lo stesso
+numero di vocali. Scrivere una funzione che prenda in
+input una matrice A di puntatori a stringhe, e
+restituisca un boolean che indichi se esiste una
+colonna di A in cui sono puntate almeno due stringhe
+sorelle.*/
+
+#include <stdio.h>
