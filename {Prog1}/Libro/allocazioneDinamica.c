b6a52683647a68323c7ef548453da0e9d17c81c6
SamuExe14 <samueleleoonardi14@gmail.com>
2023-12-06 12:25:30 +0100

    Modificato allocazione dinamica

--- {Prog1}/Libro/allocazioneDinamica.c
+++ {Prog1}/Libro/allocazioneDinamica.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main(int *argc, char **argv){
-//Allocazione statica: è
-//Allocazione dinamica:
-//Allocazione automatica:
+//* Allocazione statica: è la classica allocazione di memoria che avviene quando si dichiara una variabile
+//* Allocazione dinamica: allocazione di memoria che avviene durante l'esecuzione del programma
+//* Allocazione automatica: allocazione di memoria che avviene quando si dichiara una variabile all'interno di una funzione
+//!MALLOC E FREE LEZIONE
 
 malloc(); //alloca dinamicamente un blocco di memoria
 
@@ -18,4 +19,12 @@ newPtr = malloc(sizeof(struct node)); //?--> Alloca dinamicamente un blocco di m
                                    //?--> se non è disponibile un blocco di memoria sufficiente, restituisce NULL
 
 free(newPtr); //?--> Dealloca il blocco di memoria puntato da newPtr
+
+//!CALLOC E REALLOC
+
+void *calloc(size_t nmemb, size_t size); //?--> Alloca dinamicamente un blocco di memoria equivalente a nmemb * size
+
+//? calloc e malloc svolgono la stessa funzione, ma calloc azzera la memoria che alloca
+
+void realloc(void *ptr, size_t size); //?--> Rialloca dinamicamente un blocco di memoria puntato da ptr, di dimensione size
 }
