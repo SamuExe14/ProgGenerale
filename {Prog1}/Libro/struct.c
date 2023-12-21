d1ac57792e658b93f82f5a1c3e3615dcccb96091
SamuExe14 <samueleleoonardi14@gmail.com>
2023-12-21 19:27:15 +0100

    Creato struct.c

---
+++ {Prog1}/Libro/struct.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+//* Le strutture sono usate comunemente per definire record
+//* di dati da memorizzare in un file
+
+struct card
+{ //? card è l'ETICHETTA della struttura
+     char *face;
+     char *suit; //? gli elementi della struttura sono chiamati MEMBRI
+
+}; //? --> è previsto il ; per la terminazione della struct
+
+struct employee
+{
+     char firstName[20];
+     char lastName[20];
+     unsigned int age;
+     char gender;
+     double hourlySalary;
+};
+
+//* E' possibile creare una struct autoreferenziale( una struct che ha come membro una struct differente)
+
+struct employee2
+{
+     char firstName[20];
+     char lastName[20];
+     unsigned int age;
+     char gender;
+     double hourlySalary;
+     struct employee teamLeaderPtr; //! E' un errore 
+     struct employee *teamLeaderPtr;
+};
