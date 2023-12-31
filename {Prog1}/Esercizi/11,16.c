2251f059c7a549e9b0e76e6e364ef91b2ab2e35b
SamuExe14 <samu@PCoddio.home-life.hub>
2023-12-31 17:19:58 +0100

    Creato file dataStruct.c

--- {Prog1}/Esercizi/11,16.c
+++ {Prog1}/Esercizi/11,16.c
@@ -10,18 +10,18 @@ int main()
      }
      else
      {
-          fprintf(filePtr, "%+18s%+18s\n", "Data type","Size");
-          fprintf(filePtr, "%+18s%+18u\n","char", sizeof(char));
-          fprintf(filePtr, "%+18s%+18u\n","unsigned char", sizeof(unsigned char));
-          fprintf(filePtr, "%+18s%+18u\n","short int", sizeof(short int));
-          fprintf(filePtr, "%+18s%+18u\n","unsigned short int", sizeof(unsigned short int));
-          fprintf(filePtr, "%+18s%+18u\n","int", sizeof(int));
-          fprintf(filePtr, "%+18s%+18u\n","unsigned int", sizeof(unsigned int));
-          fprintf(filePtr, "%+18s%+18u\n","long int", sizeof(long int));
-          fprintf(filePtr, "%+18s%+18u\n","unsigned long int", sizeof(unsigned long int));
-          fprintf(filePtr, "%+18s%+18u\n","float", sizeof(float));
-          fprintf(filePtr, "%+18s%+18u\n","double", sizeof(double));
-          fprintf(filePtr, "%+18s%+18u\n","long double", sizeof(long double));
+          fprintf(filePtr, "%+18s%+18s\n", "Data type", "Size");
+          fprintf(filePtr, "%+18s%+18u\n", "char", sizeof(char));
+          fprintf(filePtr, "%+18s%+18u\n", "unsigned char", sizeof(unsigned char));
+          fprintf(filePtr, "%+18s%+18u\n", "short int", sizeof(short int));
+          fprintf(filePtr, "%+18s%+18u\n", "unsigned short int", sizeof(unsigned short int));
+          fprintf(filePtr, "%+18s%+18u\n", "int", sizeof(int));
+          fprintf(filePtr, "%+18s%+18u\n", "unsigned int", sizeof(unsigned int));
+          fprintf(filePtr, "%+18s%+18u\n", "long int", sizeof(long int));
+          fprintf(filePtr, "%+18s%+18u\n", "unsigned long int", sizeof(unsigned long int));
+          fprintf(filePtr, "%+18s%+18u\n", "float", sizeof(float));
+          fprintf(filePtr, "%+18s%+18u\n", "double", sizeof(double));
+          fprintf(filePtr, "%+18s%+18u\n", "long double", sizeof(long double));
           // fprintf(filePtr,"", sizeof());
      }
 }
---
+++ {Prog1}/Libro/dataStruct.c
@@ -0,0 +1,52 @@
+//! STRUTTURE DINAMICHE DI DATI
+#include <stdio.h>
+#include <stdlib.h>
+
+//! LISTE COLLEGATE
+//* Sono collezioni di dati allineati in una riga e le inserzioni e cancellazioni avvengono ovunque
+//* Gli elementi di una lista vengono chiamati nodi connessi da puntatori di collegamento (link). 
+//* Si accede a un elemento di una lista tramite un puntatore al primo nodo della lista e successivamente mediante gli altri puntatori
+//* Per convenzione il puntatore di collegamento dell'ultimo nodo viene posto a NULL per segnare la fine della lista
+//* Una lista collegata è appropriata quando il numero degli elementi da rappresentare non è prevedibile, infatti
+// struttura autoreferenziale
+struct listNode{
+     char data; 
+     struct listNode *nextPtr; // puntatore al nodo successivo
+};
+
+typedef struct listNode ListNode; // sinonimo per struct listNode
+typedef ListNode *ListNodePtr;
+
+void insert(ListNodePtr *sPtr, char value);
+char delete(ListNode *sPtr, char value);
+int isEmpty(ListNodePtr sPtr);
+void printList(ListNodePtr currentPtr);
+void instructions(void);
+
+int main(){
+     ListNodePtr startPtr = NULL; // ancora non ci sono nodi nella lista
+     char item; // char inserito dall'utente
+}
+
+//! PILE 
+//* Le inserzioni e le cancellazioni avvengono sono nella sua cima
+//! CODE
+//* Le inserzioni vengono fatte nella sua testa e le rimozioni vengono fatte nella sua coda
+//! ALBERI BINARI
+
+
+
+//! ALLOCAZIONE DINAMICA DI MEMORIA
+//* Per mantenere delle strutture dinamiche di dati che possono crescere e diminuire all'esecuzione si richiede un'allocazione di memoria dinamica.
+//* Per effettuare questa operazione sono essenziali la malloc e l'operatore sizeof().
+
+// struct node {
+//      int data;
+//      struct node *nextPtr; // *nextPtr è chiamato link perchè può essere usato per legare una struct di tipo struct node a un'altra dello stesso tipo
+// };
+
+// int main(){
+// myPtr = malloc(sizeof(struct node)); //? Alloca dinamicamente una nuova area in memoria equivalente al sizeof(struct node) e memorizza un puntatore alla memoria allocata in newPtr.
+//                                       //? Se la memoria non viene allocata malloc restituisce NULL
+// free(myPtr); //? Libera la memoria e viene restituita al sistema 
+// }
