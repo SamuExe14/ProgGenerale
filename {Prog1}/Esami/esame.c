eebf6416c5c58597a7596e396c2dbfafc5ab97f2
samu.exe <samueleleoonardi14@gmail.com>
2024-02-12 09:39:15 +0100

    Eliminati file Esame

---
+++ {Prog1}/Esami/esame.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "esame.h"
+
+struct values{
+   int n;
+   int m;
+   int p;
+};
+
+int main(int argc, char *argv[])
+{
+   readInput(argc, argv);
+
+}
