d50b68a2d8d231f5cd7d43a582639f78259f32b7
samu <samu@PCoddio.home-life.hub>
2024-04-16 12:05:08 +0200

    Rimosso pdf libroProg

---
+++ Prog2/Libro/cap2.cpp
@@ -0,0 +1,20 @@
+// #include <cstdio>
+#include <iostream>
+using namespace std;
+
+
+int main(){
+     enum Colori {Blu, Arancione, Viola}; // definisco con enum un nuovo tipo che può avere i seguenti valori
+
+     Colori mioColore = Blu; // definisco una variabile con il tipo definito con enum
+
+     enum Semaforo {Verde, Giallo = 10, Rosso};
+
+     // Esercizio 2.3
+
+     cout << "Oggi è giorno 14 aprile" << endl;
+
+     // Esercizio 2.4
+     cout << '\n' <<"Benvenuti in C++" << '\n' << "Cominceremo presto a programmare" << endl;
+     
+}
