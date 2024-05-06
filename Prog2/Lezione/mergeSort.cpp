cd88a452daca64051e93f4fdd9260a5ace481aa7
samu.exe <samueleleoonardi14@gmail.com>
2024-05-06 17:04:38 +0200

    Aggiunti file mergeSort e quickSort

---
+++ Prog2/Lezione/mergeSort.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+// usa l'algoritmo divide et impera
+// l'input di dimensione n viene partizionato in due parti di lunghezza n/2
+// le due sottosequenze vengono ordinate in maniera ricorsiva fino a quando si ottengono delle sequenze composte da un solo elemento
+// a questo punto il merge unisce due sottosequenze ordinate
+//? TODO: completare il file con il codice del libro
+int main()
+{
+}
---
+++ Prog2/Lezione/quickSort.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+// è uno degli algoritmi più efficienti e si basa sulla divisione del vettore in tre partizioni:
+// · Centrale: contenente un solo elemento detto pivot
+// · Sinistra: contenente tutti gli elementi minori di pivot
+// · Destra: contenente tutti gli elementi maggiori di pivot
+//? TODO: completare il file con il codice del libro
+int main()
+{
+}
