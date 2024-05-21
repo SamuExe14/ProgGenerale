6458eedd823bde941d047294ff4fd3610f4616c7
Samux_14 <samueleleoonardi14@gmail.com>
2024-05-21 11:17:58 +0200

    Rimpiazzato capitolo12

--- Prog2/Libro/cap12.cpp
+++ Prog2/Libro/cap12.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 //! CLASSI DERIVATE
@@ -61,17 +62,65 @@ public:
 class A
 {
 public:
-     A() { cout << "Costruttore A" << endl; }
+     A() { cout << "Allocato costruttore A " << endl; }
 };
 
-class B: public A
+class B : public A
 {
 public:
-     B() { cout << "Costruttore B" << endl; }
+     B() { cout << "Allocato costruttore B" << endl; }
 };
+
+class C : public B
+{
+public:
+     C() { cout << "Allocato costruttore C" << endl; }
+     ~C() { cout << "Distrutto C" << endl; }
+};
+
+//! ESEMPIO 12.6
+
+class Punto2D
+{
+public:
+     Punto2D(int x, int y)
+     {
+          ascissa = x;
+          ordinata = y;
+     };
+     int ascissa;
+     int ordinata;
+};
+
+class Punto3D : public Punto2D
+{
+public:
+     Punto3D(int, int, int);
+     float modulo();
+
+private:
+     int quota;
+};
+
+Punto3D::Punto3D(int x, int y, int z) : Punto2D(x, y)
+{
+     quota = z;
+}
+
+float Punto3D::modulo()
+{
+     int r = ascissa * ascissa + ordinata * ordinata + quota * quota;
+     return sqrt(r);
+}
 
 int main()
 {
      Libro libro;
      B b;
+
+     C costruttore_C;
+
+     Punto3D p(10, 20, 30);
+     cout << p.modulo();
+     return 0;
 }
