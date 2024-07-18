cc0c6d54034ea1eebd54d36ae75fb981cdab0908
Samux_14 <samueleleoonardi14@gmail.com>
2024-07-18 19:49:50 +0200

    Modificato cap6.cpp

--- Prog2/Libro/cap6.cpp
+++ Prog2/Libro/cap6.cpp
@@ -12,14 +12,26 @@ struct impiegato //* È possibile costruire una struct annidata, alla quale si p
 {
      // persona persona;
      float salario;
+     struct persona;
 };
+
+struct CD
+{
+     char titolo[20];
+     char artista[20];
+     int num_canzoni;
+     float prezzo;
+     char data_compra[8];
+} cd1, cd2;
 
 int main()
 {
      persona persona1; //* Si può definire una variabile struct in questo modo
      persona samuele = {"Samuele", "Leonardi", 20};
-     cout << samuele.nome << samuele.cognome << samuele.età << endl;
+     cout << samuele.nome << " "<< samuele.cognome << " "<< samuele.età << endl;
      impiegato impiegato1;
+
+     CD cd1; //! È possibile creare una struct senza includere la parola struct 
 
      //! ARRAY DI STRUCT
      persona persone[100];
@@ -28,5 +40,5 @@ int main()
      persone[0].cognome = "Leonardi";
      persone[0].età = 20;
 
-     cout << persone[0].nome;
+     cout << persone[0].nome << endl;
 }
